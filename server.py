dd4a899a143557c47da9777213d332e3383bd40c
gadife <gadi.fe@gmail.com>
2016-03-27 23:55:16 +0300

    add server.py

---
+++ server.py
@@ -0,0 +1,74 @@
+import cherrypy
+import socket
+import base64
+import json
+import time
+import requests
+import os
+
+class Root(object):
+    @cherrypy.expose
+    def index(self):
+        return """
+        <html><body>
+            <h2>Upload a file</h2>
+            <form action="upload" method="post" enctype="multipart/form-data">
+            filename: <input type="file" name="myFile" /><br />
+            <input type="submit" />
+            </form>
+        </body></html>
+        """
+
+    def upload(self, myFile):
+        out = """<html>
+        <body>
+ 	     %s
+        </body>
+        </html>"""
+
+        newFile = open("/home/web/pics/"+str(myFile.filename), 'a+')
+        while True:
+             data = myFile.file.read()
+             if not data:
+                 break
+             newFile.write(data)
+        newFile.close
+
+        image_file = "/home/web/pics/"+str(myFile.filename)
+        before_b64 = open(image_file)
+        after_b64 = base64.b64encode(before_b64.read())
+        timestr = time.strftime("%Y%m%d-%H%M%S")
+
+        file_name = timestr + ".json"
+	my_dict = {                   
+	  'requests': [
+	  {
+	      'image': {
+		'content': after_b64
+		},
+	      'features': [
+	      {
+		'type':"LABEL_DETECTION",
+		'maxResults': 1
+	      },
+	      {
+		'type':"FACE_DETECTION",
+		'maxResults':1
+	      }	
+	      ]
+	  }
+	]
+	}
+
+	out_file = open(file_name,"w")
+	json.dump(my_dict,out_file, indent=4)
+	out_file.close()
+
+	data = open(file_name, "rb").read()
+	response = requests.post(url='https://vision.googleapis.com/v1/images:annotate?key=AIzaSyBToFRO9Q4aWzNIjM9J7uxUgPh9sqL6iXs',data=data,headers={'Content-Type': 'application/json'})
+
+        return out % (response.texr)
+    upload.exposed = True
+
+cherrypy.config.update({'server.socket_host': '0.0.0.0'})
+cherrypy.tree.mount(Root())
